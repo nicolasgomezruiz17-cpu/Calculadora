14a940cf043ec0e840ecca03b4a7a350c81689d8
Nicolas <nicolasgomezruiz17@gmail.com>
2026-01-16 10:29:57 -0300

    put arraylist

--- Calculadora/src/Suma.java
+++ Calculadora/src/Suma.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
+
 public class Suma {
     int num1;
     int num2;
-
+    ArrayList<String> historial = new ArrayList<>();
     public void suma(int num1, int num2){
         System.out.println("El resultado de la operación es: " + (num1+num2));
     }
